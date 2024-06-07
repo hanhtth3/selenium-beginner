3b95882d5f024dcf59b84e589aba1e5c1b1b294f
hanhtth3 <hanhtth3@gmail.com>
2024-06-07 16:27:05 +0700

    maintain: clean old code

--- src/test/java/com/baymax/sky/ui/GoogleSearchPage.java
+++
@@ -1,8 +0,0 @@
-package com.baymax.sky.ui;
-
-import net.serenitybdd.annotations.DefaultUrl;
-import net.serenitybdd.core.pages.PageObject;
-
-@DefaultUrl("http://www.google.co.uk")
-public class GoogleSearchPage extends PageObject {}
-
